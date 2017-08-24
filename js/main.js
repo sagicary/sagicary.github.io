5b7140fca538c872cb1e790e01388414fbc360a7
sagicary <sagicary@gmail.com>
2017-08-24 02:33:20 -0400

    Arreglando detalles

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*Para Nav*/
+$("#main").click(function() {
+  $("#mini-fab").toggleClass('hidden');
+});
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();  
+});
+
+/*Tooltip*/
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
